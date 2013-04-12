144cc860e215b6a3bcbcccfb895bc8cbb2548eaf
Kelley Lange <kelley@Kelley-Langes-iMac.local>
2013-04-11 22:58:47 -0500

    added graphics to represent numbers < not happy with this method of implementation

--- BasicMath/BasicMathViewController.h
+++ BasicMath/BasicMathViewController.h
@@ -10,8 +10,48 @@
 
 @interface BasicMathViewController : UIViewController <UIPickerViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UILabel *notification;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView3;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView4;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView5;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView6;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView7;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView8;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView9;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView10;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView11;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView12;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView13;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView14;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView15;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView16;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView17;
+
+@property (weak, nonatomic) IBOutlet UIImageView * imageView18;
+
+
+
+
+
+
+@property (weak, nonatomic) IBOutlet UILabel *notification;
 
 @property (weak, nonatomic) IBOutlet UILabel *topNum;
 
--- BasicMath/BasicMathViewController.m
+++ BasicMath/BasicMathViewController.m
@@ -25,16 +25,91 @@
 @synthesize botNum;
 @synthesize getRand = _getRand;
 @synthesize pickerView;
-
+@synthesize imageView1;
+@synthesize imageView2;
+@synthesize imageView3;
+@synthesize imageView4;
+@synthesize imageView5;
+@synthesize imageView6;
+@synthesize imageView7;
+@synthesize imageView8;
+@synthesize imageView9;
+@synthesize imageView10;
+@synthesize imageView11;
+@synthesize imageView12;
+@synthesize imageView13;
+@synthesize imageView14;
+@synthesize imageView15;
+@synthesize imageView16;
+@synthesize imageView17;
+@synthesize imageView18;
 
 - (IBAction)next
 {
+    self.clearImageNumbers;
     self.topNumValue = self.getNum2;
     self.botNumValue = self.getNum2;
     topNum.text = [NSString stringWithFormat:@"%i", self.topNumValue];
     botNum.text = [NSString stringWithFormat:@"%i", self.botNumValue];
     self.answer = 0;
     notification.text = @"";
+    self.imageNumbers;
+}
+
+-(void)imageNumbers
+{
+    int x = self.topNumValue;
+    int y = self.botNumValue;
+    NSLog(@"x = %i", x);
+    NSLog(@"y = %i", y);
+    
+    UIImage *image1 = [UIImage imageNamed:@"apple.png"];
+    UIImage *image2 = [UIImage imageNamed:@"grapes.png"];
+    
+    if (x >= 1) imageView1.image = image1;
+    if (x >= 2) imageView2.image = image1;
+    if (x >= 3) imageView3.image = image1;
+    if (x >= 4) imageView4.image = image1;
+    if (x >= 5) imageView5.image = image1;
+    if (x >= 6) imageView6.image = image1;
+    if (x >= 7) imageView7.image = image1;
+    if (x >= 8) imageView8.image = image1;
+    if (x >= 9) imageView9.image = image1;
+    
+    if (y >= 1) imageView10.image = image2;
+    if (y >= 2) imageView11.image = image2;
+    if (y >= 3) imageView12.image = image2;
+    if (y >= 4) imageView13.image = image2;
+    if (y >= 5) imageView14.image = image2;
+    if (y >= 6) imageView15.image = image2;
+    if (y >= 7) imageView16.image = image2;
+    if (y >= 8) imageView17.image = image2;
+    if (y >= 9) imageView18.image = image2;
+}
+
+-(void)clearImageNumbers
+{
+    UIImage *imageBlank = [UIImage imageNamed:@""];
+    
+    imageView1.image = imageBlank;
+    imageView2.image = imageBlank;
+    imageView3.image = imageBlank;
+    imageView4.image = imageBlank;
+    imageView5.image = imageBlank;
+    imageView6.image = imageBlank;
+    imageView7.image = imageBlank;
+    imageView8.image = imageBlank;
+    imageView9.image = imageBlank;
+    imageView10.image = imageBlank;
+    imageView11.image = imageBlank;
+    imageView12.image = imageBlank;
+    imageView13.image = imageBlank;
+    imageView14.image = imageBlank;
+    imageView15.image = imageBlank;
+    imageView16.image = imageBlank;
+    imageView17.image = imageBlank;
+    imageView18.image = imageBlank;
+
 }
 
 - (IBAction)checkAnswer
